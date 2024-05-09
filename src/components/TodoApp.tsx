0bf3f1f1dc60c325b2896d282477081f070c21f7
yvromero <yvonne.romero@uds.edu.py>
2024-05-09 00:27:37 -0400

    Modify the color of the button and add flexbox layout in CSS

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -21,7 +21,7 @@ export const TodoApp = () => {
     return (
         <div>
             <h1>Lista de Tareas</h1>
-            <div>
+            <div className='flex'>
                 <input 
                 type="text"
                 value={nuevaTarea}
